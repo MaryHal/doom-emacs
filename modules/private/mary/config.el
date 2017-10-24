2a76f9645c6b15985c182dbe3d23cdcb51d8afa7
MaryHal <maryhal@users.noreply.github.com>
2017-10-23 21:52:47 -0700

    "Personal" config

---
+++ modules/private/mary/config.el
@@ -0,0 +1,21 @@
+;;; private/mary/config.el -*- lexical-binding: t; -*-
+
+;; No line numbers
+(setq doom-line-numbers-style nil)
+
+(setq doom-font
+      (font-spec :family "Inziu Iosevka Slab J"
+                 :size 12))
+(setq doom-big-font
+      (font-spec :family "Inziu Iosevka Slab J"
+                 :size 18))
+(setq doom-variable-pitch-font
+      (font-spec :family "Inziu Roboto J"
+                 :size 12))
+(setq doom-unicode-font
+      (font-spec :family "Noto Sans Mono CJK JP"
+                 :size 12))
+
+(when (featurep! :feature evil)
+  (load! +bindings)  ; my key bindings
+  (load! +commands)) ; my custom ex commands
