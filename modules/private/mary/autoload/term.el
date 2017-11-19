7e64af61f2975a8665e4a163db01b3e4fdb77efc
MaryHal <MaryHal@users.noreply.github.com>
2017-11-19 13:37:43 -0800

    mary:term:open-terminal use getenv instead of eval

--- modules/private/mary/autoload/term.el
+++ modules/private/mary/autoload/term.el
@@ -2,6 +2,6 @@
 (defun +mary:term:open-terminal ()
   "Just open a terminal in the cwd using the $TERMINAL environment variable."
   (interactive)
-  (if (string= system-type "gnu/linux")
-      (call-process-shell-command "eval $TERMINAL" nil 0)
+  (if IS-LINUX
+      (call-process-shell-command (getenv "TERMINAL") nil 0)
     (call-process-shell-command "start powershell.exe" nil 0)))
